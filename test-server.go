a4360026ab4ab1071ba5b2ade7a6e3ac4f28ea1a
David Wallace <david.wallace@gmail.com>
2000-01-01 00:29:44 +0000

    test server lights board LED

--- test-server.go
+++ test-server.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-    "time"
-    "strconv"
 	"os"
+	"strconv"
+	"time"
 )
 
 func setLED(ledNum int, val int) {
-    ledFn := fmt.Sprintf("/sys/class/leds/beaglebone:green:usr%d/brightness", ledNum)
-    fmt.Println(ledFn)
+	ledFn := fmt.Sprintf("/sys/class/leds/beaglebone:green:usr%d/brightness", ledNum)
+	fmt.Println(ledFn)
 
 	// open output file
 	ledFile, err := os.Create(ledFn)
@@ -51,31 +51,31 @@ func spiThread(pixelsToSend chan []byte, sendingIsDone chan int) {
 		}
 	}()
 
-    flipper := 0
+	flipper := 0
 	for pixels := range pixelsToSend {
 		fmt.Println("[send] starting to send", len(pixels), "values")
 
-        setLED(0, flipper)
-        flipper = 1 - flipper
+		setLED(0, flipper)
+		flipper = 1 - flipper
 
-        bytes := make([]byte, 0)
+		bytes := make([]byte, 0)
 
 		// zeros
 		numZeroes := (len(pixels) / 32) + 2
 		for ii := 0; ii < numZeroes*5; ii++ {
-            bytes = append(bytes, 0)
+			bytes = append(bytes, 0)
 		}
 
 		// pixels
 		for _, v := range pixels {
 			// high bit is always on, remaining seven bits are data
 			v2 := 128 | (v >> 1)
-            bytes = append(bytes, v2)
+			bytes = append(bytes, v2)
 		}
 
 		// final zero
-        bytes = append(bytes, 0)
-        sendBytes(spiFile, bytes)
+		bytes = append(bytes, 0)
+		sendBytes(spiFile, bytes)
 
 		sendingIsDone <- 1
 	}
@@ -88,16 +88,16 @@ func main() {
 	sendingIsDone := make(chan int, 0)
 	go spiThread(pixelsToSend, sendingIsDone)
 
-    for ii := 0; true; ii = (ii + 1) % 256 {
-        pixels := []byte{255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0}
-        pixels[9] = byte((ii*9) % 256)
-        pixels[10] = byte((ii*9) % 256)
-        pixels[11] = byte((ii*9) % 256)
-        fmt.Println("[main] pixels =", pixels)
-        pixelsToSend <- pixels
-        <-sendingIsDone
-        time.Sleep(300 * time.Millisecond)
-    }
+	for ii := 0; true; ii = (ii + 1) % 256 {
+		pixels := []byte{255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0}
+		pixels[9] = byte((ii * 9) % 256)
+		pixels[10] = byte((ii * 9) % 256)
+		pixels[11] = byte((ii * 9) % 256)
+		fmt.Println("[main] pixels =", pixels)
+		pixelsToSend <- pixels
+		<-sendingIsDone
+		time.Sleep(300 * time.Millisecond)
+	}
 
 	fmt.Println("--------------------------------------------------------------------------------/")
 }
