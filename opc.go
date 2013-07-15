d363f2091c097ee665f642c42eb0afd3a9fb397f
David Wallace <david.wallace@gmail.com>
2013-07-15 00:54:02 -0700

    print fps

--- opc.go
+++ opc.go
@@ -1,26 +1,40 @@
 package main
 
 import "fmt"
-//import "bitbucket.org/davidwallace/go-opc/colorutils"
+import "time"
+import "bitbucket.org/davidwallace/go-opc/colorutils"
 
 func main() {
 
-    const N_PIXELS = 10
+	const N_PIXELS = 1000
+	var array = make([]byte, N_PIXELS*3)
 
-    var array = make([]int, N_PIXELS*3)
+	var pct, r, g, b, t float64
+	var last_print = float64(time.Now().UnixNano()) / 1.0e9
+	var frames = 0
+	for true {
+		t = float64(time.Now().UnixNano()) / 1.0e9
+		frames += 1
+		if t > last_print+1 {
+			last_print = t
+			fmt.Printf("%f ms (%d fps)\n", 1000.0/float64(frames), frames)
+			frames = 0
+		}
+		for ii := 0; ii < N_PIXELS; ii++ {
+			pct = float64(ii) / N_PIXELS
 
-    var pct, r, g, b float64
-    for ii := 0; ii < N_PIXELS; ii++ {
-        pct = float64(ii) / N_PIXELS
-        r = pct
-        g = pct
-        b = pct
-        array[ii*3 + 0] = int(r*255)
-        array[ii*3 + 1] = int(g*255)
-        array[ii*3 + 2] = int(b*255)
-    }
-    for ii,v := range array {
-        fmt.Printf("array[%d] = %d\n", ii, v)
-    }
+			r = pct
+			g = pct
+			b = pct
+
+			array[ii*3+0] = colorutils.FloatToByte(r)
+			array[ii*3+1] = colorutils.FloatToByte(g)
+			array[ii*3+2] = colorutils.FloatToByte(b)
+		}
+
+		//for ii, v := range array {
+		//    fmt.Printf("array[%d] = %d\n", ii, v)
+		//}
+	}
 
 }
