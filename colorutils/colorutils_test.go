42b0b674eb151e6976a49afbb1e4251918af7bfe
David Wallace <david.wallace@gmail.com>
2013-07-14 21:30:10 -0700

    add colorutils_test.go

--- colorutils/colorutils_test.go
+++ colorutils/colorutils_test.go
@@ -7,7 +7,6 @@ func remapTestHelper(t *testing.T, x, oldmin, oldmax, newmin, newmax, result flo
 		t.Errorf("Remap(%f,%f,%f,%f,%f) = %f, want %f", x, oldmin, oldmax, newmin, newmax, tmp, result)
 	}
 }
-
 func TestRemap(t *testing.T) {
 	// x, oldmin, oldmax, newmin, newmax, result
 	remapTestHelper(t, -1.0, 0.0, 1.0, 0.0, 1.0, -1.0)
@@ -21,4 +20,31 @@ func TestRemap(t *testing.T) {
 	remapTestHelper(t, 0.8, 0.0, 1.0, 10.0, 20.0, 18.0)
 	remapTestHelper(t, 1.0, 0.0, 1.0, 10.0, 20.0, 20.0)
 	remapTestHelper(t, 2.0, 0.0, 1.0, 10.0, 20.0, 30.0)
+
+	remapTestHelper(t, 0.0, 10.0, 20.0, 0.0, 1.0, -1.0)
+	remapTestHelper(t, 10.0, 10.0, 20.0, 0.0, 1.0, 0.0)
+	remapTestHelper(t, 18.0, 10.0, 20.0, 0.0, 1.0, 0.8)
+	remapTestHelper(t, 20.0, 10.0, 20.0, 0.0, 1.0, 1.0)
+	remapTestHelper(t, 30.0, 10.0, 20.0, 0.0, 1.0, 2.0)
+
+	// degenerate input range
+	remapTestHelper(t, 11.0, 11.0, 11.0, 10.0, 20.0, 15.0)
+	remapTestHelper(t, 19.0, 11.0, 11.0, 10.0, 20.0, 15.0)
+
+	// degenerate output range
+	remapTestHelper(t, 20.0, 10.0, 20.0, 1.0, 1.0, 1.0)
+}
+
+func clampTestHelper(t *testing.T, x, minn, maxx, result float64) {
+	if tmp := Clamp(x, minn, maxx); tmp != result {
+		t.Errorf("Clamp(%f,%f,%f) = %f, want %f", x, minn, maxx, tmp, result)
+	}
+}
+func TestClamp(t *testing.T) {
+	// x, minn, maxx
+	clampTestHelper(t, -1.0, 0.0, 1.0, 0.0)
+	clampTestHelper(t, 0.0, 0.0, 1.0, 0.0)
+	clampTestHelper(t, 0.5, 0.0, 1.0, 0.5)
+	clampTestHelper(t, 1.0, 0.0, 1.0, 1.0)
+	clampTestHelper(t, 2.0, 0.0, 1.0, 1.0)
 }
