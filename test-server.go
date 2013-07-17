869824256b4f93d46134b56af01e786070182215
David Wallace <david.wallace@gmail.com>
2013-07-17 13:10:47 -0700

    Clean up test-server.go

--- test-server.go
+++ test-server.go
@@ -1,5 +1,10 @@
 package main
 
+// This is a quick attempt to send pixels via SPI to LED strips
+// using the LPD8806 chipset, like the ones available from Adafruit.
+
+// This has not been tested yet.
+
 import (
 	"fmt"
 	"os"
@@ -7,7 +12,10 @@ import (
 	"time"
 )
 
-func setLED(ledNum int, val int) {
+// Set one of the on-board LEDs on the Beaglebone.
+//    ledNum: between 0 and 3 inclusive
+//    val: 0 or 1.
+func setOnboardLED(ledNum int, val int) {
 	ledFn := fmt.Sprintf("/sys/class/leds/beaglebone:green:usr%d/brightness", ledNum)
 	fmt.Println(ledFn)
 
@@ -28,6 +36,7 @@ func setLED(ledNum int, val int) {
 	}
 }
 
+// Write the byte slice to the open file descriptor
 func sendBytes(fd *os.File, bytes []byte) {
 	fmt.Println("[sendBytes]", bytes)
 	if _, err := fd.Write(bytes); err != nil {
@@ -35,11 +44,16 @@ func sendBytes(fd *os.File, bytes []byte) {
 	}
 }
 
+// Recieve byte slices over the pixelsToSend channel.
+// When we get one, write it to the SPI file descriptor and toggle one
+//  of the Beaglebone's onboard LEDs.
+// After sending the frame, send 1 over the sendingIsDone channel.
+// The byte slice should hold values from 0 to 255 in [r g b  r g b  r g b  ... ] order.
 func spiThread(pixelsToSend chan []byte, sendingIsDone chan int) {
 
 	spiFn := "/dev/spidev1.0"
 
-	// open output file
+	// open output file and keep the file descriptor around
 	spiFile, err := os.Create(spiFn)
 	if err != nil {
 		panic(err)
@@ -52,15 +66,18 @@ func spiThread(pixelsToSend chan []byte, sendingIsDone chan int) {
 	}()
 
 	flipper := 0
+	// as we get byte slices over the channel...
 	for pixels := range pixelsToSend {
 		fmt.Println("[send] starting to send", len(pixels), "values")
 
-		setLED(0, flipper)
+		// toggle onboard LED
+		setOnboardLED(0, flipper)
 		flipper = 1 - flipper
 
+		// build a new slice of bytes in the format the LED strand wants
 		bytes := make([]byte, 0)
 
-		// zeros
+		// leading zeros to begin a new frame of pixels
 		numZeroes := (len(pixels) / 32) + 2
 		for ii := 0; ii < numZeroes*5; ii++ {
 			bytes = append(bytes, 0)
@@ -68,12 +85,12 @@ func spiThread(pixelsToSend chan []byte, sendingIsDone chan int) {
 
 		// pixels
 		for _, v := range pixels {
-			// high bit is always on, remaining seven bits are data
+			// high bit must be always on, remaining seven bits are data
 			v2 := 128 | (v >> 1)
 			bytes = append(bytes, v2)
 		}
 
-		// final zero
+		// final zero to latch the last pixel
 		bytes = append(bytes, 0)
 		sendBytes(spiFile, bytes)
 
@@ -86,8 +103,10 @@ func main() {
 
 	pixelsToSend := make(chan []byte, 0)
 	sendingIsDone := make(chan int, 0)
+
 	go spiThread(pixelsToSend, sendingIsDone)
 
+	// send some test data
 	for ii := 0; true; ii = (ii + 1) % 256 {
 		pixels := []byte{255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0}
 		pixels[9] = byte((ii * 9) % 256)
