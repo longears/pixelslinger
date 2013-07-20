c8ba2dc1a12f4645f10a02588b443657e89720b4
David Wallace <david.wallace@gmail.com>
2013-07-20 15:57:01 -0700

    update off.sh after refactor

--- off.sh
+++ off.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # wall.json is the pattern with the most LEDs so we can just always use that one
-./off layouts/wall.json
+./main --layout layouts/wall.json --source off --dest spi --once
 
