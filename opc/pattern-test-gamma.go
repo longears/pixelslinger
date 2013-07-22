dc8baaff8a7843046be3788b0c13cfb09480de90
David Wallace <david.wallace@gmail.com>
2013-07-22 08:19:28 +0000

    test-gamma: use much faster PosMod2 instead of PosMod

--- opc/pattern-test-gamma.go
+++ opc/pattern-test-gamma.go
@@ -5,7 +5,6 @@ package opc
 
 import (
 	"bitbucket.org/davidwallace/go-metal/colorutils"
-	"math"
 	"time"
 )
 
@@ -14,20 +13,29 @@ func MakePatternTestGamma(locations []float64) ByteThread {
 		for bytes := range bytesIn {
 			n_pixels := len(bytes) / 3
 			t := float64(time.Now().UnixNano())/1.0e9 - 9.4e8
-			_ = t
 
 			// fill in bytes array
 			var r, g, b float64
 			for ii := 0; ii < n_pixels; ii++ {
 
 				// make moving rgb sawtooth waves
-				r = colorutils.PosMod(float64(ii)-t*2.84, 16) / 15.0
-				g = colorutils.PosMod(float64(ii)-t*4.00, 19) / 18.0
-				b = colorutils.PosMod(float64(ii)-t*5.37, 27) / 26.0
+				r = colorutils.PosMod2(float64(ii)-t*2.84, 16) / 15.0
+				g = colorutils.PosMod2(float64(ii)-t*4.00, 19) / 18.0
+				b = colorutils.PosMod2(float64(ii)-t*5.37, 27) / 26.0
 				// convert sawtooth waves into triangle waves
-				r = math.Abs(r*2 - 1)
-				g = math.Abs(g*2 - 1)
-				b = math.Abs(b*2 - 1)
+				r = r*2 - 1
+				g = g*2 - 1
+				b = b*2 - 1
+				if r < 0 {
+					r *= -1
+				}
+				if g < 0 {
+					g *= -1
+				}
+				if b < 0 {
+					b *= -1
+				}
+				// monochrome region in first 32 LEDs
 				if ii < 32 {
 					g = r
 					b = r
