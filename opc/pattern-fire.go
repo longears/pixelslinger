0bd6adc1d9dccfc65f6ef8e99bda374d4215b591
David Wallace <david.wallace@gmail.com>
2013-07-29 00:24:02 -0700

    clean up fire pattern a bit (but do not run go fmt on it)

--- opc/pattern-fire.go
+++ opc/pattern-fire.go
@@ -24,9 +24,9 @@ func MakePatternFire(locations []float64) ByteThread {
             var max_coord_x, max_coord_y, max_coord_z float64
             var min_coord_x, min_coord_y, min_coord_z float64
             for ii := 0; ii < n_pixels; ii++ {
-                x := locations[ii*3 + 0]
-                y := locations[ii*3 + 1]
-                z := locations[ii*3 + 2]
+                x := locations[ii*3+0]
+                y := locations[ii*3+1]
+                z := locations[ii*3+2]
                 if ii == 0 || x > max_coord_x { max_coord_x = x }
                 if ii == 0 || y > max_coord_y { max_coord_y = y }
                 if ii == 0 || z > max_coord_z { max_coord_z = z }
@@ -57,40 +57,39 @@ func MakePatternFire(locations []float64) ByteThread {
 
                 // apply various wiggles to coordinate space
                 // offset, period, min, max
-                zp1 := (  zp + colorutils.Cos2(xp, t*0.33 + 8.63, 0.15 * 1.7, 0, 1) * 0.2 +
-                              colorutils.Cos2(xp, -t*0.23 + 2.43, 0.34 * 1.7, 0, 1) * 0.3  )
-
+                zp1 := (  zp + colorutils.Cos2(xp,  t*0.33 + 8.63, 0.15 * 1.7, 0, 1) * 0.2 +
+                               colorutils.Cos2(xp, -t*0.23 + 2.43, 0.34 * 1.7, 0, 1) * 0.3  )
                 //zp2 := (  zp + colorutils.Cos2(xp, -t*0.38 + 1.23, 0.23 * 1.7, 0, 1) * 0.2 +
-                //              colorutils.Cos2(xp, t*0.23 + 2.53, 0.63 * 1.7, 0, 1) * 0.3  )
+                //               colorutils.Cos2(xp,  t*0.23 + 2.53, 0.63 * 1.7, 0, 1) * 0.3  )
                 zp3 := (  zp + colorutils.Cos2(xp, -t*0.42 + 5.62, 0.27 * 1.7, 0, 1) * 0.2 +
-                              colorutils.Cos2(xp, t*0.20 + 3.07, 0.55 * 1.7, 0, 1) * 0.3  )
-                zp4 := (  zp + colorutils.Cos2(xp, t*0.36 + 4.81, 0.20 * 1.7, 0, 1) * 0.2 +
-                              colorutils.Cos2(xp, -t*0.26 + 7.94, 0.67 * 1.7, 0, 1) * 0.3  )
+                               colorutils.Cos2(xp,  t*0.20 + 3.07, 0.55 * 1.7, 0, 1) * 0.3  )
+                zp4 := (  zp + colorutils.Cos2(xp,  t*0.36 + 4.81, 0.20 * 1.7, 0, 1) * 0.2 +
+                               colorutils.Cos2(xp, -t*0.26 + 7.94, 0.67 * 1.7, 0, 1) * 0.3  )
 
                 // make basic vertical gradient
                 vgrad := colorutils.Cos2(colorutils.Clamp(zp, 0, 1), 0, 2, 0, 1)
                 // vgrad := 1 - colorutils.Clamp(zp, 0, 1)
 
                 // smallest fastest noise
-                noise_lit := (   colorutils.Cos2(xp,  -4.37 * t/4, 0.21, 0, 1) +
-                                 colorutils.Cos2(yp,   4.37 * t/4, 0.21, 0, 1) +
-                                 colorutils.Cos2(zp1,  4.37 * t,   0.21, 0, 1)  ) / 3
+                noise_lit := (  colorutils.Cos2(xp,  -4.37 * t/4, 0.21, 0, 1) +
+                                colorutils.Cos2(yp,   4.37 * t/4, 0.21, 0, 1) +
+                                colorutils.Cos2(zp1,  4.37 * t,   0.21, 0, 1)  ) / 3
 
                 // small fast noise
-                noise_med := (   colorutils.Cos2(xp,  -3 * t/4, 0.3, 0, 1) +
-                                 colorutils.Cos2(yp,   3 * t/4, 0.3, 0, 1) +
-                                 colorutils.Cos2(zp3,  3 * t,   0.3, 0, 1)  ) / 3
+                noise_med := (  colorutils.Cos2(xp,  -3 * t/4, 0.3, 0, 1) +
+                                colorutils.Cos2(yp,   3 * t/4, 0.3, 0, 1) +
+                                colorutils.Cos2(zp3,  3 * t,   0.3, 0, 1)  ) / 3
 
                 // big slow noise
-                noise_big := (   colorutils.Cos2(xp,  -0.9 * t/2, 0.8, 0, 1) +
-                                 colorutils.Cos2(yp,   0.9 * t/2, 0.8, 0, 1) +
-                                 colorutils.Cos2(zp4,  0.9 * t,   0.8, 0, 1)  ) / 3
+                noise_big := (  colorutils.Cos2(xp,  -0.9 * t/2, 0.8, 0, 1) +
+                                colorutils.Cos2(yp,   0.9 * t/2, 0.8, 0, 1) +
+                                colorutils.Cos2(zp4,  0.9 * t,   0.8, 0, 1)  ) / 3
 
                 // combine vgradient with noise
-                v := (   vgrad  +
-                         colorutils.Remap(noise_lit, 0,1, -1,1)*0.17 +
-                         colorutils.Remap(noise_med, 0,1, -1,1)*0.2 +
-                         colorutils.Remap(noise_big, 0,1, -1,1)*0.8   )
+                v := (  vgrad  +
+                        colorutils.Remap(noise_lit, 0,1, -1,1)*0.17 +
+                        colorutils.Remap(noise_med, 0,1, -1,1)*0.20 +
+                        colorutils.Remap(noise_big, 0,1, -1,1)*0.80  )
 
                 // apply sine contrast curve
                 //v = colorutils.Cos2( colorutils.Clamp(v,0,1), 0, 2, 1, 0 )
