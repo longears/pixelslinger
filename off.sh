e34004d5655f25b64bfb4d39ffdf70f834e0a8d1
David Wallace <david.wallace@gmail.com>
2013-07-30 19:10:22 -0700

    update off.sh

--- off.sh
+++ off.sh
@@ -6,7 +6,7 @@ echo 1 > /sys/class/leds/beaglebone\:green\:usr2/brightness
 echo 1 > /sys/class/leds/beaglebone\:green\:usr3/brightness
 
 # wall.json is the pattern with the most LEDs so we can just always use that one
-./main --layout layouts/wall.json --source off --dest spi --once
+./pixelslinger --layout layouts/wall.json --source off --dest spi --once
 
 echo 0 > /sys/class/leds/beaglebone\:green\:usr0/brightness
 echo 0 > /sys/class/leds/beaglebone\:green\:usr1/brightness
