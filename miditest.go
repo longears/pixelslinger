fdd98b4b06c7d25085792c76c041e416b94160e1
David Wallace <david.wallace@gmail.com>
2013-07-30 20:20:42 -0700

    Bring miditest.go up to date with recent changes to midi/midi.go

---
+++ miditest.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"github.com/longears/pixelslinger/midi"
+)
+
+func main() {
+	fmt.Println("-------------------------------------------------------")
+	midiMessageChan := midi.GetMidiMessageStream("/dev/midi1")
+    for midiMessage := range midiMessageChan {
+        fmt.Println(midiMessage)
+    }
+}
