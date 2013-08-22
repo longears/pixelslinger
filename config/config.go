a23f1edff9251ecf51d9ad894643fdfca82b04b9
David Wallace <david.wallace@gmail.com>
2013-08-22 00:21:26 -0700

    Add new knob names to config.go

--- config/config.go
+++ config/config.go
@@ -6,8 +6,9 @@ import (
 
 // midi pads
 const (
-	FLASH_PAD = midi.LPD8_PAD1
+	FLASH_PAD   = midi.LPD8_PAD1
 	TWINKLE_PAD = midi.LPD8_PAD2
+	RIPPLE_PAD  = midi.LPD8_PAD3 // todo
 )
 
 // midi knobs
@@ -15,7 +16,8 @@ const (
 	GAIN_KNOB   = midi.LPD8_KNOB1
 	EYELID_KNOB = midi.LPD8_KNOB2
 	SPEED_KNOB  = midi.LPD8_KNOB3
-	SWITCH_KNOB  = midi.LPD8_KNOB4
+	SWITCH_KNOB = midi.LPD8_KNOB4
+	MORPH_KNOB  = midi.LPD8_KNOB5 // todo
 )
 
 // knob starting values before they have been moved
@@ -27,5 +29,7 @@ func init() {
 		GAIN_KNOB:   127,
 		EYELID_KNOB: 127,
 		SPEED_KNOB:  63,
+		SWITCH_KNOB: 0,
+		MORPH_KNOB:  0,
 	}
 }
